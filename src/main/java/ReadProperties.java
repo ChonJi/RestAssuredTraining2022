7e42fa14bc245a8db7d81e2513ba403c2c4ece42
ChonJi <palladine@tlen.pl>
2022-03-28 11:42:44 +0200

    Added pom properties handler for user key and token mask on repository

---
+++ src/main/java/ReadProperties.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadProperties {
+
+    private final Properties properties;
+
+    public ReadProperties() {
+        properties = new Properties();
+        try (final InputStream is = ReadProperties.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readBaseUri() {
+        return properties.getProperty("baseUri");
+    }
+
+    public String readKey() {
+        return properties.getProperty("key");
+    }
+
+    public String readToken() {
+        return properties.getProperty("token");
+    }
+
+}
